e04d0148b75ed3577daadb898d3b8c5b994c7e19
Paolo Marcheschi <paolo.marcheschi@gmail.com>
2024-04-08 13:22:52 +0200

    Update _updatePublisher.bat
    update new publisher

--- _updatePublisher.bat
+++ _updatePublisher.bat
@@ -146,7 +146,7 @@ GOTO end
 
 :scripts
 
-REM Download all batch files (and this one with a new name) tru to make it work
+REM Download all batch files (and this one with a new name) tru to make it work possibly
 
 SETLOCAL DisableDelayedExpansion
 
